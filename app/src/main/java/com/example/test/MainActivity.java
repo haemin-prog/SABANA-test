b76ee1de3393db121656c07348d9e26386115b83
haemin-prog <haemin.lab@gmail.com>
2021-05-26 10:43:03 +0900

    AddMarketMarker 추가

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +12,8 @@ import android.util.Log;
 import android.widget.LinearLayout;
 
 import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapMarkerItem;
+import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 import java.util.List;
@@ -28,8 +32,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
     double initialLatitude = 37.451711825486946;
     double initialLongitude = 126.65661030197585;
 
-    public List<Market> marketList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -38,7 +40,23 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
         // T Map View
         tMapView = new TMapView(this);
+        initTMapView();
 
+        // T Map GPS
+        initTMapGPS();
+
+        // Load Database (Market)
+        List<Market> marketList = initLoadMarketDatabase();
+
+        // Add Market Marker
+        addMarketMarker(marketList);
+
+        // T Map View Using Linear Layout
+        LinearLayout linearLayoutTMap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        linearLayoutTMap.addView(tMapView);
+    }
+
+    public void initTMapView(){
         // API Key
         tMapView.setSKTMapApiKey(API_Key);
 
@@ -51,11 +69,9 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         // Initial Location Setting
         tMapView.setLocationPoint(initialLongitude, initialLatitude);
         tMapView.setCenterPoint(initialLongitude, initialLatitude);
+    }
 
-        // T Map View Using Linear Layout
-        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
-        linearLayoutTmap.addView(tMapView);
-
+    public void initTMapGPS(){
         // Request For GPS permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -72,10 +88,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
         // Using GPS
         tMapGPS.OpenGps();
-
-        // Load Initial Database (Market)
-        initLoadMarketDatabase();
-
     }
 
     @Override
@@ -84,16 +96,51 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         tMapView.setCenterPoint(location.getLongitude(), location.getLatitude());
     }
 
-    // Load Database
-    private void initLoadMarketDatabase(){
+    public List<Market> initLoadMarketDatabase(){
 
         DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
         databaseHelper.OpenDatabaseFile();
 
-        marketList = databaseHelper.getTableData();
+        List<Market> marketList = databaseHelper.getTableData();
         Log.e("test", String.valueOf(marketList.size()));
 
         databaseHelper.close();
+        return marketList;
+    }
+
+    public void addMarketMarker(List<Market> marketList){
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.markerline_yellow);
+
+        for (int i = 0; i < marketList.size(); i++){
+
+            String storeName = marketList.get(i).storeName;     // 이름
+            String address = marketList.get(i).address;         // 주소
+            double lat = marketList.get(i).latitude;            // 위도
+            double lon = marketList.get(i).longitude;           // 경도
+
+            // TMapPoint
+            TMapPoint tMapPoint = new TMapPoint(lat, lon);
+
+            // TMapMarkerItem
+            // Marker Initial Settings
+            TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
+            tMapMarkerItem.setIcon(bitmap);
+            tMapMarkerItem.setPosition(0.5f, 1.0f);
+            tMapMarkerItem.setTMapPoint(tMapPoint);
+            tMapMarkerItem.setName(storeName);
+
+            // Balloon View Initial Settings
+            tMapMarkerItem.setCanShowCallout(true);     // Balloon View 사용
+            tMapMarkerItem.setCalloutTitle(storeName);  // Main Message
+            tMapMarkerItem.setCalloutSubTitle(address); // Sub Message
+            tMapMarkerItem.setAutoCalloutVisible(false); // 초기 접속 시 Balloon View X
+
+            // add Marker on T Map View
+            // id로 마커를 식별
+            tMapView.addMarkerItem("marketLocation" + i, tMapMarkerItem);
+        }
+
     }
 
 }
